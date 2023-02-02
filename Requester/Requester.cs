461c76164e1e8db7729a1c9c27d471a7bff27779
alaraalara <alaraozkutucu@gmail.com>
2023-02-02 10:37:57 -0500

    fixed some of the errors regarding empty bodies

--- Requester/Requester.cs
+++ Requester/Requester.cs
@@ -18,6 +18,8 @@ using System.Text.RegularExpressions;
 using System.Reflection;
 using System.Net.Http.Headers;
 using Azure.Core;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Requester
 {
@@ -25,6 +27,7 @@ namespace Requester
     {
         public List<Log> ListOfRequests;
         string portNumber;
+        public Dictionary<string, string> ResponseBodies;
 
         public Requester(List<Log> ListOfRequests, string portNumber=null) { 
             this.ListOfRequests = ListOfRequests;   
@@ -36,6 +39,7 @@ namespace Requester
             {
                 this.portNumber = ":" + portNumber;
             }
+            ResponseBodies = new Dictionary<string, string>();  
         }
 
         //NOTE Might be edge cases where bunch of collection of both rest and oatuh type of different session id requests might be executed!
@@ -43,14 +47,14 @@ namespace Requester
         {
             //can be 2 login requests too. It should check for every login requests. This only assume 1 login request and its session id.
             System.Guid? LoginProcGUID = ListOfRequests.Where(r => r.Path!=null && r.Path.ToLower().Contains("login")).First().ProcGuid;
-            var cookies = ListOfRequests.Where(r => r.ProcGuid == LoginProcGUID && r.EventType == 2).First().Cookies;
+            var cookies = ListOfRequests.Where(r => r.ProcGuid == LoginProcGUID && r.EventType == 2).First().Headers;
             string sessionID = null;
-            if (cookies != null)
+            if (cookies != null && cookies.Contains("ASP.NET_SessionId:"))
             {
                 sessionID = cookies.Substring(cookies.IndexOf("ASP.NET_SessionId:")).Split(',')[0];
                 Console.WriteLine(sessionID);
             }
-            if (ListOfRequests.Where(r => r.Cookies != null && r.Cookies.IndexOf("ASP.NET_SessionId:") != -1).All(r => r.Cookies.Substring(r.Cookies.IndexOf("ASP.NET_SessionId:")).Split(',')[0] == sessionID))
+            if (ListOfRequests.Where(r => r.Headers != null && r.Headers.IndexOf("ASP.NET_SessionId:") != -1).All(r => r.Headers.Substring(r.Headers.IndexOf("ASP.NET_SessionId:")).Split(',')[0] == sessionID))
             {
                 return true;
             }
@@ -103,42 +107,69 @@ namespace Requester
             }
         }
 
-
+        //TODO: has to check for error responses! WHAT TO DO FOR THEM
         //NOTE TODO: Url might change too! depending on the previous body
-        public Log ChangeOldRequest(Log OldPreviousResponse, Log Request, string NewPreviousResponse)
+        private Log ChangeRequestBody(string OldPreviousResponse, string NewPreviousResponse, Log Request, int index=-1)
         {
-            if (OldPreviousResponse.Body == null || NewPreviousResponse == null || Request.Body == null)
+            if (Request.Body.IsNullOrEmpty())
             {
+                //if the response is null then check the previous previous response!
                 return Request;
             }
-            var OldPreviousResponseJson = GetJToken(OldPreviousResponse.Body);
-            var RequestJson = GetJToken(Request.Body);
-            var NewPreviousResponseJson = GetJToken(NewPreviousResponse);
-            var difference = new JsonDiffPatch().Diff(NewPreviousResponseJson, OldPreviousResponseJson);
-    
-            if(difference == null)
-            {
-                return Request;
+            else if(NewPreviousResponse.IsNullOrEmpty() || OldPreviousResponse.IsNullOrEmpty()) {
+                if(index == 0)
+                {
+                    return Request;
+                }
+                if(index == -1)
+                {
+                    index = ResponseBodies.Count()-1;//last element
+                }
+                else
+                {
+                    index = index - 1;
+                }
+                ChangeRequestBody(ResponseBodies.ElementAt(index).Key, ResponseBodies.ElementAt(index).Value, Request, index);                
             }
-            else  
+            try
             {
-                CompareJsonBody(OldPreviousResponseJson, NewPreviousResponseJson, RequestJson);
+                var OldPreviousResponseJson = GetJToken(OldPreviousResponse);
+                var RequestJson = GetJToken(Request.Body);
+                var NewPreviousResponseJson = GetJToken(NewPreviousResponse);
+                var difference = new JsonDiffPatch().Diff(NewPreviousResponseJson, OldPreviousResponseJson);
 
+                if (difference == null)
+                {
+                    return Request;
+                }
+                else
+                {
+                    CompareJsonBody(OldPreviousResponseJson, NewPreviousResponseJson, RequestJson);
+
+                }
+                ResponseBodies.Add(OldPreviousResponse, NewPreviousResponse);
+                Request.Body = RequestJson.ToString();
+                Console.WriteLine(Request.Body);
+                return Request;
             }
-            Request.Body = RequestJson.ToString();
-            Console.WriteLine(Request.Body);
-            return Request;
+            catch(Exception e) {
+                
+                return Request;
+            }
+            
+            
         }
 
         
         private void CompareJsonBody(JToken OldPreviousResponseJson, JToken NewPreviousResponseJson, JToken newReqJson)
         {
-            var tokensResponse = JsonExtensions.FindTokens(OldPreviousResponseJson, "value").Concat(JsonExtensions.FindTokens(OldPreviousResponseJson, "id"));
+            var tokensResponse = JsonExtensions.FindTokens(OldPreviousResponseJson, "id").Concat(JsonExtensions.FindTokens(OldPreviousResponseJson, "value"));
             foreach (var token in tokensResponse)
             {
+                var tokenNew = NewPreviousResponseJson.SelectToken(token.Path);
                 if (JToken.DeepEquals(NewPreviousResponseJson.SelectToken(token.Path), token) == false)
                 {
-                    if ((newReqJson.SelectToken(token.Path)) != null)
+                    if (newReqJson.SelectToken(token.Path) != null)
                     {
                         newReqJson.SetByPath(token.Path, NewPreviousResponseJson.SelectToken(token.Path));
                     }
@@ -165,7 +196,48 @@ namespace Requester
             }
         }
         
-        public string CompareQueryStringAndResponse(Log PreviousResponse, Log NewResponse, string Cookies)
+        public string GetNewPath(string path, string oldResponse, string newResponse, int index=-1)
+        {
+            
+            if(oldResponse.IsNullOrEmpty() || newResponse.IsNullOrEmpty())
+            {
+                if(ResponseBodies.Count() == 0)
+                {
+                    return path;
+                }
+                if (index == -1)
+                {
+                    index = ResponseBodies.Count() - 1;//last element
+                }
+                else
+                {
+                    index = index - 1;
+                }
+                return GetNewPath(path, ResponseBodies.ElementAt(index).Key, ResponseBodies.ElementAt(index).Value, index);
+            }
+            var difference = new JsonDiffPatch().Diff(oldResponse, newResponse);
+            if (difference.IsNullOrEmpty())
+            {
+                return path;
+            }  
+            //0th position is the old value, and 1st position is the new value
+            var tokens = JsonExtensions.FindTokens(JToken.Parse(difference), "value").Concat(JsonExtensions.FindTokens(JToken.Parse(difference), "id"))
+                .Concat(JsonExtensions.FindTokens(JToken.Parse(difference), "files:put")).Concat(JsonExtensions.FindTokens(JToken.Parse(difference), "self"));
+            var pathList = path.Split('/');
+            foreach(var token in tokens) // will the end value be always in token??????????
+            {
+                
+                if (token != null && token.ToArray().Length > 1 && pathList.Contains(token[0].ToString()))
+                {
+
+                    path = path.Replace(token[0].ToString(), token[1].ToString());
+                }
+            }
+            return path;
+
+        }
+
+        private string CompareQueryStringAndResponse(Log PreviousResponse, Log NewResponse, string Cookies)
         {
             string a = "?fileID=aa015587-d4e0-47f6-84f5-42fc5f9ff870";
             string b = "?$expand=Address&$top=1000&$skip=0&$custom=&$filter=Type%20eq%20'Contact'";
@@ -231,12 +303,22 @@ namespace Requester
 
         public void Execute()
         {
+            
             var client = new HttpMethods(this.portNumber);
-            ReExecuteRequests(client);
             if (ListOfRequests.Any(log => log.Path != null && log.Path.ToLower().Contains("login")))
             {
+                //this design looks weird!
                 var login = ListOfRequests.Where(log => log.Path.Contains("login")).First();
-                client.Logout(login);
+                try
+                {
+                    ReExecuteRequests(client);
+                }
+                catch (Exception e)
+                {
+                    client.Logout(login);
+
+                }
+
             }
             
         }
@@ -258,12 +340,11 @@ namespace Requester
             //make helper private method to handle login request and execute oauth and rest from there 
             if (ListOfRequests.Any(log => log.Path!=null && log.Path.ToLower().Contains("login")))
             {
-                login = ListOfRequests.Where(log => log.Path.Contains("login")).First();
+                login = ListOfRequests.Where(log => log.Path.Contains("login") && log.EventType==1).First();
             }
             if(login != null)
             {
                 var loginResponse = client.HttpCall(login).Result;
-                
                 if (loginResponse != null && (int)loginResponse.StatusCode == 204)
                 {
                     HttpResponseMessage? NewPreviousResponse =null;
@@ -278,9 +359,13 @@ namespace Requester
                             if (NewPreviousResponse != null && OldPreviousResponseLog!=null)
                             {
                                 //data.response has to be the prev response before data.request
-                                NewRequest = ChangeOldRequest(OldPreviousResponseLog, data.Request, NewPreviousResponse.Content.ReadAsStringAsync().Result);
-                                Console.WriteLine(NewRequest.Body);
+                                NewRequest.Path = GetNewPath(NewRequest.Path, OldPreviousResponseLog.Body, NewPreviousResponse.Content.ReadAsStringAsync().Result);
                             }
+                            if(NewPreviousResponse!=null)
+                            {
+                                NewRequest = ChangeRequestBody(OldPreviousResponseLog.Body, NewPreviousResponse.Content.ReadAsStringAsync().Result, data.Request);
+                            }
+
                             NewPreviousResponse = client.HttpCall(NewRequest).Result;
                             OldPreviousResponseLog = data.Response;
                             Console.WriteLine(NewPreviousResponse.StatusCode);
